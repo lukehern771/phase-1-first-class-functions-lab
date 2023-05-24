fd10caf7eb6f3054e851f779c110aff70138d91b
Luke <hluke9242@gmail.con>
2023-05-24 13:59:08 -0600

    phase-1 complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (){
+    return ['Antonia', 'Nuru']
+}
+
+const returnLastTwoDrivers = function (){
+    return ['Amari', 'Mo']
+}
+
+const selectingDrivers  = [returnFirstTwoDrivers, returnLastTwoDrivers] ;
+
+const createFareMultiplier = function (fareMultiplier){
+    return function(value) {
+        return fareMultiplier * value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, functions){
+    return functions(drivers)
+    
+}
